07f538332617e93c9fd499e67a9c2661c4ffeabf
Damaris <osiagodamaris@gmail.com>
2026-04-09 20:48:49 +0300

    complete tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const taskForm = document.getElementById('task-form')
+const taskInput = document.getElementById('task-input')
+const taskList = document.getElementById('task-list')
+
+taskForm.addEventListener('submit', function(event) {
+    event.preventDefault()  // stops page from refreshing
+
+    const taskText = taskInput.value
+
+    const li = document.createElement('li')
+    li.textContent = taskText
+
+    taskList.appendChild(li)
+
+    taskInput.value = ''  // clears the input after submission
+})
