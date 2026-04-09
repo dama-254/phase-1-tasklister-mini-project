5a8752310a7fe89bfddc3c2923f668b5154e7417
Damaris <osiagodamaris@gmail.com>
2026-04-09 21:10:12 +0300

    complete tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
+const taskForm = document.getElementById('create-task-form')
+console.log('form:', taskForm) // should NOT be null
 
-document.addEventListener('DOMContentLoaded', function() {
-    const taskForm = document.getElementById('task-form')
-    const taskInput = document.getElementById('task-input')
-    const taskList = document.getElementById('task-list')
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
 
-    taskForm.addEventListener('submit', function(event) {
-        event.preventDefault()
+    const task = event.target['new-task-description'].value
+    console.log('task entered:', task) // should show what you typed
 
-        const taskText = taskInput.value
-
-        const li = document.createElement('li')
-        li.textContent = taskText
-
-        taskList.appendChild(li)
-
-        taskInput.value = ''
-    })
+    buildToDo(task)
 })
+
+function buildToDo(task) {
+    const li = document.createElement('li')
+    li.textContent = task
+
+    const taskList = document.getElementById('task')
+    console.log('task list:', taskList) // should NOT be null
+    taskList.appendChild(li)
+}
